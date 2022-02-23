f62ca3a96016d17d6ccb127597a37b2e572de5e8
Erich <eheiseler@gmail.com>
2022-02-22 19:13:08 -0500

    commit tree 839e5115a3dc9455978386c60fcc37d94826a637

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  const free = "This one is on me!"
+  const charged= "I will gladly take your thirty bucks."
+  const notAllowed = "No can do."
+  if(ride <= 400){
+    return free
+  }
+  else if (ride > 2000  && ride < 2500){
+    return charged
+  }
+  else {
+    return notAllowed
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isNYC = "Ok, sounds good."
+  const noGo = "No go."
+  return (city === "NYC" ? isNYC: noGo)
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    break
+    case 'not as generous':
+      return "Thank you."
+    break
+    case 'thanks for everything':
+      return "Bye."
+    break
+  }
 }
